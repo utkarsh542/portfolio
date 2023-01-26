3eabdf8d7690d0fc6938c230e07ec9a627c934ef
utkarsh <gutkarsh191@gmail.com>
2023-01-26 20:06:30 +0530

    profile pic correction

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row ">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white mt-80 mx-3 " >
+          <h2 className="text-4xl sm:text-7xl font-bold text-white mt-64 mx-3 " >
             I'm Utkarsh Gupta
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
