7a75f1c60a3c650a9fcedddebaa2257be7be61b5
utkarsh <gutkarsh191@gmail.com>
2023-01-26 20:10:21 +0530

    add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row ">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white mt-64 mx-3 " >
+          <h2 className="text-4xl sm:text-7xl font-bold text-white mt-60 mx-3 " >
             I'm Utkarsh Gupta
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
@@ -37,7 +37,7 @@ const Home = () => {
           <img
             src={HeroImage}
             alt="my profile"
-            className="rounded-full mx-auto w-2/3 md:w-full mt-44"
+            className="rounded-full mx-auto w-2/3 md:w-full mt-36"
           />
         </div>
       </div>
